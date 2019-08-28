db2d6e45f2763d786dcec9d23daf2bf23f9096f5
Bliprog <40035648+Bliprog@users.noreply.github.com>
2019-08-29 02:41:37 +0300

    add requir

--- TestTask/settings.py
+++ TestTask/settings.py
@@ -138,4 +138,4 @@ LOGIN_REDIRECT_URL = '/logined'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','https://guarded-cliffs-30265.herokuapp.com']
