491a681d102557cebd5af6cdfe90a140439eb5ed
Bliprog <40035648+Bliprog@users.noreply.github.com>
2019-08-29 04:09:37 +0300

    add requir

--- TestTask/wsgi.py
+++ TestTask/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+import os
+
 from django.core.wsgi import get_wsgi_application
-from whitenoise.base import WhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestTask.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
